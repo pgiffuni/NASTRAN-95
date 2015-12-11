d38634be73014054740dd8721b376d4b940ffd30
Thomas Wuerfl <thomaswuerfl@gmx.de>
2015-12-11 19:28:17 +0100

    corrected ETIME call

--- sb/nastim.f
+++ sb/nastim.f
@@ -1,7 +1,7 @@
       SUBROUTINE NASTIM (IHR, IMN, ISC, CPUSEC)
       REAL ARRAY(2)
-      CALL ETIME(ARRAY)
-      SECS   = ARRAY(2)
+      REAL RESULT
+      CALL ETIME(ARRAY,RESULT)      SECS   = ARRAY(2)
       IHR    = SECS / 3600.
       IMN    = ( SECS - 3600.*IHR ) / 60.
       ISC    = SECS - ( 3600.*IHR ) - ( 60.*IMN )
