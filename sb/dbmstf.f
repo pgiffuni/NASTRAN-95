2eb623c0a0dc26e29fcf18fa61e486012ea5297f
Thomas Wuerfl <thomaswuerfl@gmx.de>
2015-12-11 19:49:02 +0100

    changed WRITE FORMAT from I8 to I16

--- sb/dbmstf.f
+++ sb/dbmstf.f
@@ -11,19 +11,20 @@
       IF ( IDBDIR .NE. 0 ) WRITE( LOUT, 902 ) IMEMNU
 901   FORMAT(1H1
      &,5X,'STATISTICS ON IN-MEMORY DATA BASE AND DISK I/O USAGE',/
-     &,/,8X,' LENGTH (IN WORDS) OF OPEN CORE ALLOCATED          ',I8
-     &,/,8X,' LENGTH (IN WORDS) OF IN-MEMORY DATA BASE ALLOCATED',I8
-     &,/,8X,' NUMBER OF BLOCKS USED IN THE IN-MEMORY DATA BASE  ',I8
-     &,/,8X,' NUMBER OF BLOCKS ALLOCATED FOR THE IN-MEMORY DATA ',I8
-     &,/,8X,' PERCENTAGE OF IN-MEMORY DATA USED                 ',I8,'%'
-     &,/,8X,' TOTAL BLOCKS WRITTEN TO DISK                      ',I8
-     &,/,8X,' BLOCK SIZE (IN WORDS)                             ',I8
-     &,/,8X,' NUMBER OF OPENS TO DISK FILES                     ',I8
-     &,/,8X,' NUMBER OF CLOSES TO DISK FILES                    ',I8
-     &,/,8X,' NUMBER OF WRITES TO DISK FILES                    ',I8
-     &,/,8X,' NUMBER OF READS FROM DISK FILES                   ',I8)
+     &,/,8X,' LENGTH (IN WORDS) OF OPEN CORE ALLOCATED          ',I16
+     &,/,8X,' LENGTH (IN WORDS) OF IN-MEMORY DATA BASE ALLOCATED',I16
+     &,/,8X,' NUMBER OF BLOCKS USED IN THE IN-MEMORY DATA BASE  ',I16
+     &,/,8X,' NUMBER OF BLOCKS ALLOCATED FOR THE IN-MEMORY DATA ',I16
+     &,/,8X,' PERCENTAGE OF IN-MEMORY DATA USED                 ',I16
+     &,'%'
+     &,/,8X,' TOTAL BLOCKS WRITTEN TO DISK                      ',I16
+     &,/,8X,' BLOCK SIZE (IN WORDS)                             ',I16
+     &,/,8X,' NUMBER OF OPENS TO DISK FILES                     ',I16
+     &,/,8X,' NUMBER OF CLOSES TO DISK FILES                    ',I16
+     &,/,8X,' NUMBER OF WRITES TO DISK FILES                    ',I16
+     &,/,8X,' NUMBER OF READS FROM DISK FILES                   ',I16)
 902   FORMAT(
-     &   8X,' MEMORY (IN WORDS) NOT USED BY IN-MEM. DATA BASE   ',I8
+     &   8X,' MEMORY (IN WORDS) NOT USED BY IN-MEM. DATA BASE   ',I16
      & )
       RETURN
       END
