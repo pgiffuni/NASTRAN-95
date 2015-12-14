7a558080655b9019165003e6bde1f10be812f242
Thomas Wuerfl <thomaswuerfl@gmx.de>
2015-12-14 20:36:31 +0100

    Removed unneeded files.

--- f90/to_be_fixed/smcccd.f
+++
@@ -1,9 +0,0 @@
-      SUBROUTINE SMCCCD ( DTEMP, ZIL, ILIM, ZOL )
-      DOUBLE COMPLEX      DTEMP( ILIM ), ZIL( ILIM ), ZOL
-      DO 10 I = 1, ILIM
-      DTEMP( I ) = DTEMP( I ) + ZIL( I ) * ZOL
-10    CONTINUE
-      RETURN
-      END
-      
-      
